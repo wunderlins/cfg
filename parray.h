2d364e3ad2bd85247c6dc8b70637374c5e6b6f6e
Simon Wunderlin <swunderlin@gmail.com>
2014-11-07 09:48:44 +0100

    making sure the header is included only once

---
+++ parray.h
@@ -0,0 +1,50 @@
+/**
+ * dynamically growing pointer array
+ *
+ * GOAL:
+ * create a set of functions which will allocate a pointer array. if
+ * methods add items, the main array should grow to the desired size.
+ *
+ *
+ * (c) 2014, Simon Wunderlin <simonATwunderlinDTnet>
+ */
+
+
+#ifndef __PARRAY_H__
+
+#define PARRAY_ERR_MAX 6
+#define PARRAY_ERR_LENGTH 50
+
+/**
+ * Error messages
+ */
+const char _parray_errstr[PARRAY_ERR_MAX][PARRAY_ERR_LENGTH] = {
+	/*  0 */ "",
+	/*  1 */ "Failed to allocate memory",
+	/*  2 */ "_parray_expand failed",
+	/*  3 */ "Internal string length too short for input",
+	/*  4 */ "remove failed, index larger than array size",
+	/*  5 */ "Unknown error number",
+};
+
+int _parray_errno = 0;
+
+/**
+ * This structure holds an array of pointers including the number of items
+ */
+typedef struct {
+	size_t _psize;    // size of pointer
+	size_t length;    // number of elements
+	size_t allocated; // allocated memory
+	size_t expand;    // number of elements to add if the array is full
+	void** elements;  // actual data
+} parray;
+
+parray* parray_init(size_t psize, size_t expand);
+size_t parray_set(parray* p, void* element, size_t pos); // if pos == 0, append
+const char* parray_errstr();
+//size_t parray_free(parray* p);
+
+
+#define __PARRAY_H__
+#endif
